ff24ff9203bc6452f3d8091967045ea63a84ed15
pietro ventimiglia <twntymls@gmail.com>
2019-06-05 12:53:14 +0200

    seeded the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '02 0333092',
+    category:  'italian',
+  },
+
+    {
+    name:         'miodio',
+    address:      'via della madonna',
+    phone_number: '02 0333092',
+    category:  'french',
+  },
+
+      {
+    name:         'lol',
+    address:      'via dei san giuseppe',
+    phone_number: '02 0333222',
+    category:  'italian',
+  },
+
+        {
+    name:         'napoli',
+    address:      'via napoli',
+    phone_number: '02 03330432',
+    category:  'french',
+  },
+
+
+    {
+    name:         'china town',
+    address:      'via napoli',
+    phone_number: '02 03330432',
+    category:  'chinese',
+  },
+]
+
+Restaurant.create!(restaurants_attributes)
